cc0f9a4196cd2615897c578a4df3ba6198b0029c
mharribey <mathieuharribey@hotmail.fr>
2022-04-29 15:38:26 +0200

    starter test threejs gltf

---
+++ README.md
@@ -0,0 +1,49 @@
+# Scope of this project
+
+- provide a local ThreeJS environment in which you can test your features
+
+# How to use
+
+You will need to have [nodejs](https://nodejs.org/) installed.
+
+## Installation
+
+> First, make sure that your node version is >= 14
+
+Clone the repository on your machine and move to the directory
+
+```sh
+$ git clone https://github.com/mharribey/pipe_threejs_webpack.git your_folder && cd your_folder
+```
+
+Install the packages required for the local environment
+
+```sh
+$ yarn
+```
+
+## Start local environment
+
+```sh
+$ yarn start
+```
+
+This last command will start a local http server with [live reloading](https://webpack.js.org/configuration/dev-server/#devserverlivereload) enabled so that you can iterate faster on your projects. Open [http://localhost:8080](http://localhost:8080) to see your project in the browser.
+
+## Build
+
+```sh
+$ yarn run build
+```
+
+Will bundle your js dependencies into a single minified `bundle.js` file, move your files from the `public/` to the `dist/` folder, and link the `bundle.js` with the `index.html`.
+
+**Moreover, it will create a `dist-zipped/project.zip` file which can be directly use to share your project**.
+
+# Develop your token
+
+Once the environment is started, you can edit the `src/index.js` file to start building your artwork. The `index.html` file is located in the `public/` folder.
+
+You can import libraries using `yarn` or by adding the library file in the `public/` folder and link those using relative paths in the `index.html`.
+
+Any file in the `public/` folder will be added to the final project.
---
+++ src/index.js
@@ -0,0 +1,153 @@
+// IMPORTS
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Perlin } from 'three-noise';
+import OrbitControls from 'threejs-orbit-controls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js';
+import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass.js';
+
+// HELPERS
+
+const loader = new GLTFLoader();
+const modelLoader = (url) => {
+  return new Promise((resolve, reject) => {
+    loader.load(url, data=> resolve(data), null, reject);
+  });
+}
+
+// VARIABLES
+
+let mixer, clock
+clock = new THREE.Clock();
+
+const perlin = new Perlin(Math.random())
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 8
+camera.position.y = 3
+camera.position.x = 3
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enabled = true;
+controls.enableZoom = true;
+controls.enablePan = false;
+controls.rotateSpeed = 0.5;
+controls.maxDistance = 1500;
+controls.minDistance = 0;
+
+// MODELS
+
+const loaderCube = new THREE.CubeTextureLoader();
+loaderCube.setPath( 'assets/textures/cube/' );
+const textureCube = loaderCube.load( [
+	'px.png', 'nx.png',
+	'py.png', 'ny.png',
+	'pz.png', 'nz.png'
+] );
+
+async function loadModels() {
+  // const glove = await modelLoader('/assets/models/glove/Gloves_TEST.gltf')
+  // glove.scene.scale.set(1, 1, 1)
+  // glove.scene.position.set(0,-10,0)
+
+  // glove.scene.traverse(function(el) {
+  //   if (el.type == 'Mesh') {
+  //     el.material.envMap = textureCube
+  //     el.material.envMapIntensity = 1
+  //   }
+  // })
+
+  /////////////////
+
+  // const ipod = await modelLoader('/assets/models/ipod/iPod.gltf')
+  // ipod.scene.scale.set(0.015, 0.015, 0.015)
+  // ipod.scene.position.set(-2, 1.1, 1)
+  // ipod.scene.rotation.y = Math.PI
+
+  // ipod.scene.traverse(function(el) {
+  //   if (el.type == 'Mesh') {
+  //     el.material.envMap = textureCube
+  //     el.material.envMapIntensity = 1
+  //   }
+  // })
+
+  //////////////////
+
+  const platform = await modelLoader('/assets/models/platform/scene.gltf')
+  platform.scene.scale.set(0.2, 0.2, 0.2)
+  mixer = new THREE.AnimationMixer(platform.scene);
+  platform.animations.forEach((clip) => {
+      mixer.clipAction(clip).play();
+  });                
+
+  platform.scene.traverse(function(el) {
+    if (el.type == 'Mesh') {
+      el.material.envMap = textureCube
+      el.material.envMapIntensity = 2
+    }
+  })
+
+  //////////////////
+  
+  //platform.scene.add(ipod.scene)
+  scene.add(platform.scene)
+  //scene.add(glove.scene)
+}
+
+// LIGHTS
+
+const light = new THREE.DirectionalLight( 0xFFFFFF );
+const ambientLight = new THREE.AmbientLight( 0x404040 );
+
+scene.add( light );
+scene.add( ambientLight );
+
+// POST EFFECTS IF NEEDED
+
+const composer = new EffectComposer( renderer );
+composer.addPass(new RenderPass(scene, camera));
+
+// const bloomEffect = new UnrealBloomPass(new THREE.Vector2( window.innerWidth, window.innerHeight ), 0.1, 0, 0.8 )
+// const ssaoPass = new SSAOPass( scene, camera, window.innerWidth, window.innerHeight );
+// const filmEffect = new FilmPass(.5, .5, 1, 0)
+
+// ssaoPass.kernelRadius = 51;
+// ssaoPass.minDistance = 0.012
+// ssaoPass.maxDistance = 1
+
+// composer.addPass(filmEffect); // exemple
+
+controls.update();
+function animate(dt) {
+  composer.render();
+
+  controls.update();
+
+  renderer.antialias = true;
+  renderer.setClearColor( 0xffffff, 0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+
+  const delta = clock.getDelta();
+  if ( mixer ) mixer.update( delta );
+
+	requestAnimationFrame( animate );
+}
+animate();
+
+const init = () => {
+    loadModels().catch(error => {
+      console.log(error)
+    })
+}
+init()
